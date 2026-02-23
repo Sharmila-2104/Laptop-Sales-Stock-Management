a582cb733f34192c7067de53cb263f7c39e68572
Sharmila.R <Sharmibca160@gmail.com>
2026-02-23 17:36:53 +0530

    Add files via upload

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockManagementSystem.Data;
+using StockManagementSystem.Interfaces;
+
+namespace StockManagementSystem.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _db = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+            => await _db.ToListAsync();
+
+        public async Task<T> GetByIdAsync(int id)
+            => await _db.FindAsync(id);
+
+        public async Task InsertAsync(T entity)
+            => await _db.AddAsync(entity);
+
+        public void Update(T entity)
+            => _db.Update(entity);
+
+        public void Delete(T entity)
+            => _db.Remove(entity);
+    }
+}
---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using StockManagementSystem.Data;
+using StockManagementSystem.Interfaces;
+using StockManagementSystem.Models;
+
+namespace StockManagementSystem.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public IGenericRepository<Laptop> Laptops { get; }
+        public IGenericRepository<Sale> Sales { get; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Laptops = new GenericRepository<Laptop>(context);
+            Sales = new GenericRepository<Sale>(context);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
